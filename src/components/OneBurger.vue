fcb233df388557a1f38e377485c0c4913acdd3a4
dapema01 <davidmalmberg01@gmail.com>
2024-12-03 13:36:39 +0100

    Hoppsas det funkar

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    {{ burger.name }} {{ burger.kCal }}
+  <div class="burger">
+    <section id="titleSec">
+    <h2>{{ burger.name }}</h2>
+    </section>
+    <img :src="burger.imageUrl" :alt="burger.name" />
+    <section id="infSec">
+    <p>KCal:{{ burger.kCal }}</p>
+    <p v-if="burger.gluten && burger.lactose">Contains gluten and lactose</p>
+    <p v-else-if="burger.gluten">Contains gluten</p>
+    <p v-else-if="burger.lactose">Contains lactose</p>
+    <p v-else>No gluten or lactose</p>
+    <p>Amount selected: {{ amountOrdered }}</p>
+    </section>
+    <div class="button-group">
+      <button @click="amountOrdered++">Add to order</button>
+      <button @click="amountOrdered--">Remove from order</button>
+    </div>
   </div>
 </template>
 
@@ -8,12 +23,87 @@
 export default {
   name: 'OneBurger',
   props: {
-    burger: Object
-  }
-}
+    burger:{
+      type: Object,
+      required: true,
+    },
+  },
+  
+  data: function(){
+      return {
+        amountOrdered: 0
+      };
+    },
+    watch: {
+      amountOrdered(value) {
+        if (value < 0) this.amountOrdered = 0;
+      }
+    }
+  
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.burger {
+  border-radius: 10px;
+  padding: 15px;
+  text-align: center;
+  background-color: rgb(254, 202, 150);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  width: 80%;
+  height: fit-content;
+  }
+.burger img {
+  max-width: 100%;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  align-items: center;
+}
 
+#titleSec {
+  height: 20%;
+}
+
+.burger h2 {
+  font-size: 1.5rem;
+  margin-bottom: 10px;
+  padding: 10px;
+}
+
+.burger p {
+  font-size: 1rem;
+  margin: 5px 0;
+}
+#infSec {
+  height: 20%;
+}
+
+.button-group {
+  display: flex;
+  justify-content: center;
+  gap: 10px; 
+  margin-top: 10px;
+}
+
+.button-group button {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #f0ebb3;
+  color: rgba(0, 0, 0);
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease, transform 0.2s ease;
+}
+
+.button-group button:hover {
+  
+  transform: scale(1.05); 
+}
+
+.button-group button:active {
+  background-color: #858366; 
+  transform: scale(0.95); 
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,107 @@
 <template>
-<div>
-    <div>
-    <h1>Burgers</h1>
-    <Burger v-for="burger in burgers"
-            v-bind:burger="burger" 
-            v-bind:key="burger.name"/>
-    </div>
-    <div id="map" v-on:click="addOrder">
-    click here
-    </div>
-</div>
+
+  <body>
+    <header>
+        <h1>Burgers Online</h1>
+    </header>
+<main>
+    <section id="Intro">
+        <div id="Headline">
+            <h1>Welcome to BurgerOnline</h1>
+            <img src="/img/Pubar-London-1200x630px.jpg">
+        </div>
+
+    </section>
+    <section id="tabSec">
+      <h2>Menu</h2>
+        <div class="burger-grid">
+            <Burger v-for="burger in burgers" :key="burger.name" :burger="burger" />
+        </div>
+      </section>
+    <section id="contact">
+        <form>
+            <p>
+                <label for="fullname">Full Name</label><br>
+                <input type="text" id="fullname" name="fn" v-model="formData.fullname" required="required" placeholder="Full name">
+                
+      
+            </p>
+            <p>
+                <label for="email">Email</label><br>
+                <input type="email" id="email" name="em" v-model="formData.email" required="required" placeholder="E-mail address">
+            </p>
+            <p>
+                <label for="streetname">Street Name</label><br>
+                <input type="text" id="streetname" name="sn" v-model="formData.streetname" required="required" placeholder="Street name">
+            </p>
+            <p>
+                <label for="housenumber">House Number</label><br>
+                <input type="number" id="housenumber" name="hn" v-model="formData.housenumber" required="required" placeholder="House number">
+            </p>      
+            <p>
+                <label for="payment">Payment Method</label><br>
+                <select id="paymetn" name="pm" v-model="formData.payment">
+                    <option>Visa</option>
+                    <option>Mastercard</option>
+                    <option>Klarna</option>
+                    <option selected="selected">Amex</option>
+                </select>
+            </p>      
+            
+            <p>
+                    <label for="gender">Select your gender</label><br>
+                    <input type="radio" id="man" name="gender" value="man" v-model="formData.gender">
+                    <label for="man">Man</label><br>
+                    
+                    <input type="radio" id="woman" name="gender" value="woman" v-model="formData.gender">
+                    <label for="woman">Woman</label><br>
+                    
+                    <input type="radio" id="no-gender" name="gender" value="no-gender" v-model="formData.gender">
+                    <label for="no-gender">Do not wish to provide</label>
+            </p>    
+        </form>
+
+        <div id="map-container">
+            <div id="map" v-on:click="placeDot"></div><!-- v-on:click="addOrder" -->
+            <div v-if="dot" :style="getDotStyle(dot)" class="dot"></div>
+        </div>
+            <button type="submit" @click="handleSubmit">
+                <img src="/img/png-clipart-submit-button-illustration-submit-button-icons-logos-emojis-submit-thumbnail.png">
+            </button>
+        
+    </section>
+</main>
+    <footer>
+        <br>
+        <hr>
+        &copy; 2024 BurgerOnline
+    </footer>
+  </body>
 </template>
 
 <script>
 import Burger from '../components/OneBurger.vue'
 import io from 'socket.io-client'
+import menu from '../assets/menu.json'
+
 
 const socket = io("localhost:3000");
+
+// const burgers = [
+//   new MenuItem("The Fire Burger", "/public/img/andys_burgers_original_cheeseburger_10okt23_insta.jpg", 500, true, true),
+//   new MenuItem("Cheeseburger", "/public/img/download.webp", 600, true, true),
+//   new MenuItem("Halloumi Burger", "/public/img/images.jpeg", 450, false, false)
+// ];
+
+// function MenuItem(name, imageUrl, kCal, containsGluten, containsLactose) {
+//   this.name = name;
+//   this.imageUrl = imageUrl;
+//   this.kCal = kCal;
+//   this.containsGluten = containsGluten;
+//   this.containsLactose = containsLactose;
+// }
+
+
 
 export default {
   name: 'HomeView',
@@ -25,10 +110,18 @@ export default {
   },
   data: function () {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
-                 {name: "standard burger", kCal: 450},
-                 {name: "large burger", kCal: 850}
-               ]
+      burgers: menu,
+      orderedBurgers:{},
+      location: { x: 0, y: 0 },
+      formData: {
+        fullname: "",
+        email: "",
+        streetname: "",
+        housenumber: "",
+        payment: "",
+        gender:""
+      },
+      dot: null,
     }
   },
   methods: {
@@ -44,15 +137,237 @@ export default {
                                 orderItems: ["Beans", "Curry"]
                               }
                  );
+    },
+    handleSubmit: function () {
+      console.log("Order submitted");
+      console.log(this.formData);
+    
+      if(!this.formData.fullname || !this.formData.email || !this.formData.streetname || !this.formData.housenumber || !this.formData.payment){
+        alert("Please fill in all fields");
+        console.log("Unsuccessfully");
+        return;
+      }
+      alert("Order submitted");
+      console.log(this.orderedBurgers);
+    },
+    placeDots: function (event){
+        var offset = {
+            x: event.currentTarget.getBoundingClientRect().left,
+            y: event.currentTarget.getBoundingClientRect().top
+        }
+        this.dot = {
+            x: event.clientX - offset.x,
+            y: event.clientY - offset.y
+        }
+
     }
   }
-}
+};
 </script>
 
-<style>
-  #map {
-    width: 300px;
-    height: 300px;
-    background-color: red;
-  }
+<style >
+  @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap');
+body {
+    font-size: 12pt;
+    font-family: arial;
+}
+
+p {
+    color: red;
+}
+
+h1 {
+    font-family: 'Agbalumo';
+    font-size: 36pt;
+    
+}
+main, header, footer, nav ul {
+    max-width: min(70rem, 80%);
+    margin: 0 auto 0 auto;
+}
+main {
+    background-color: bisque;
+}
+
+/* nav ul li {
+    display: inline-block;
+    background-color: grey;
+    padding: 1em;
+    margin: 1em;
+} */
+
+header {
+    display: flex;
+    background-image: url("../img/polacks.jpg");
+    background-size: cover;
+    overflow: hidden;
+    width: 100%;
+    height: 200px;
+    opacity: 0.5;
+    margin-bottom: 20px;
+}
+
+header h1 {
+    width: 40rem;
+    margin: 0 auto;
+    text-align: center;
+    padding: 30px;
+}
+
+#Intro h2, #Intro p {
+    text-align: center;
+}
+
+nav ul {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 9.25em);
+    gap: 1em;
+    padding: 0;
+}
+
+nav li {
+    display: block;
+    background-color: grey;
+    padding: 1em;
+}
+
+.Very-good {
+    color: green;
+}
+
+.Master {
+    color: green;
+    font-weight: bold;
+}
+#Headline {
+    position: relative;
+    margin: 0 auto;
+    padding: 20px 0;
+    width: 100%;
+    height: 250px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    overflow: hidden;
+}
+
+#Headline h1 {
+    position: absolute;
+    z-index: 1;
+    font-size: 2.5rem;
+    font-family: 'Agbalumo', sans-serif; 
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7); 
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+#Headline img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    opacity: 0.3; 
+}
+
+#tabSec {
+    border: 2px dashed red;
+    margin: 20px;
+}
+
+#tabSec h2 {
+  padding: 10px;
+  text-align: center;
+}
+
+
+.burger-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr; /* Responsive columns */
+  grid-gap: 20px; /* Space between grid items */
+  padding: 20px;
+  justify-items: center; /* Center the content of each grid item */
+}
+
+.Ingridients {
+    font-weight: bold;
+}
+
+#contact {
+    margin: 20px;
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+    color: white;
+    justify-content: center;
+    align-items: center;
+    border: 2px dashed white;
+    padding: 20px;
+}
+
+
+form p {
+    color: white;
+    padding: 10px 20px;
+    max-height: 100%
+}
+
+#contact button {
+    width: 200px;
+    height: 200px;
+    margin: 40px;
+    padding: 0; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none; 
+    background-color: transparent; 
+    cursor: pointer;
+}
+#contact img {
+    width: 100%; 
+    height: 100%;
+    object-fit: contain;
+    border-radius: 5px;
+}
+#contact button:hover {
+    transform: scale(1.1);
+    transition: transform 0.2s;
+}
+#map-container {
+  width: 960px; 
+  height: 539px; 
+  overflow: scroll; 
+  border: 1px solid #ccc; 
+}
+
+#map {
+  width: 1920px;
+  height: 1078px; 
+  background: url("/img/polacks.jpg") no-repeat center center;
+  background-size: cover;
+  cursor: pointer;
+}
+
+.dot {
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+}
+
+
+@media (max-width: 1024px) {
+    #tabSec {
+        height: 1800px;
+    }
+    .burger-grid {
+        grid-template-columns: 1fr;
+        height: 500px;
+    }
+    .burger-grid img {
+      
+        height: 60%;
+    }
+}
 </style>
